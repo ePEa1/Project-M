3d46c877a5aa1fbc64ce8acc17774891708004dd
ePEa_SC <zmzm5442@gmail.com>
2020-05-13 03:25:34 +0900

    AtkCollider 추가
    CharacterSystem 폴더에 들어있음

---
+++ Assets/CharacterSystem/Scripts/AtkCollider.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AtkCollider : MonoBehaviour
+{
+    public float atkDamage;
+    public float knockPower;
+    public Vector3 knockVec;
+}
