b6484839c02e7469157d01afdedf5bfd81bd32a9
yean123123 <54879918+yean123123@users.noreply.github.com>
2020-05-13 15:44:21 +0900

    monster

---
+++ Assets/Scripts/Monster/Alice/AliceDAMAGE.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AliceDAMAGE : AliceFSMState
+{
+
+    public bool IsDamaged = false;
+    public override void BeginState()
+    {
+        base.BeginState();
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (IsDamaged ==true)
+        {
+            IsDamageCheck();
+        }
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "AtkEvent")
+        {
+            IsDamaged = true;
+        }
+    }
+    void IsDamageCheck()
+    {
+        Debug.Log("Isdamage");
+
+    }
+    
+}
