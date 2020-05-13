c173c95f11cfa117738c5017c84af170299e7467
ePEa_SC <zmzm5442@gmail.com>
2020-05-13 12:11:22 +0900

    이펙트 편의성 추가

---
+++ Assets/VFX/DeleteTime.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeleteTime : MonoBehaviour
+{
+    [SerializeField] float m_lifeTime; //생존 시간
+    float m_currentTime = 0.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        m_currentTime += Time.deltaTime;
+        if (m_lifeTime <= m_currentTime)
+            Destroy(gameObject);
+    }
+}
