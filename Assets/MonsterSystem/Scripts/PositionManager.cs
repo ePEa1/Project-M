959bfdc8710ab1ba1ee9f793499c4faeacb5a2db
byss <yean123123@github.com>
2020-05-22 14:49:53 +0900

    맵밖으로 안나가게 구현/수치 수정할 수 있음

--- Assets/MonsterSystem/Scripts/PositionManager.cs
+++ Assets/MonsterSystem/Scripts/PositionManager.cs
@@ -11,6 +11,8 @@ public class PositionManager : MonoBehaviour
     public GameObject MaxZ;
     public GameObject MinZ;
 
+    public Vector3 targetPos;
+
     public float maxXVal;
     public float minXVal;
     public float maxZVal;
@@ -18,18 +20,38 @@ public class PositionManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        MaxX = transform.GetChild(0).GetComponent<GameObject>();
-        MinX = transform.GetChild(1).GetComponent<GameObject>();
-        MaxZ = transform.GetChild(2).GetComponent<GameObject>();
-        MinZ = transform.GetChild(3).GetComponent<GameObject>();
+        //MaxX = transform.GetChild(0).GetComponent<GameObject>();
+        //MinX = transform.GetChild(1).GetComponent<GameObject>();
+        //MaxZ = transform.GetChild(2).GetComponent<GameObject>();
+        //MinZ = transform.GetChild(3).GetComponent<GameObject>();
+        maxXVal = MaxX.transform.position.x;
+        minXVal = MinX.transform.position.x;
+        maxZVal = MaxZ.transform.position.z;
+        minZVal = MinZ.transform.position.z;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(target.transform.position.x > MaxX.transform.position.x)
+        targetPos = target.transform.position;
+        if(target.transform.position.x >= maxXVal)
         {
-           // target.transform.position.x = MaxX.transform.position.x;
+           target.transform.position = new Vector3(maxXVal, target.transform.position.y, target.transform.position.z);
+        }
+        if (target.transform.position.x <= minZVal)
+        {
+            target.transform.position = new Vector3(minXVal, target.transform.position.y, target.transform.position.z);
+        }
+        if (target.transform.position.z >= maxZVal)
+        {
+            target.transform.position = new Vector3(target.transform.position.x, target.transform.position.y, maxZVal);
+        }
+        if (target.transform.position.z <= minZVal)
+        {
+            target.transform.position = new Vector3(target.transform.position.x, target.transform.position.y, minZVal);
         }
     }
+
+    
 }
