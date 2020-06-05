ba1869071a2e97d6753c6c858c915ffe616a4880
byss <yean123123@github.com>
2020-06-06 03:03:00 +0900

    아아 프로젝트에서 렉걸리는 이유를 알았음
    그냥 게임 화면 존내크게해놓고 있어서그런거였음 근데 빌드는 렉걸릴듯 다른게임은 안그러는데 왜 이러냐

--- Assets/MonsterSystem/Scripts/TestBackToMain.cs
+++ Assets/MonsterSystem/Scripts/TestBackToMain.cs
@@ -8,34 +8,7 @@ public class TestBackToMain : MonoBehaviour
 {
 
     public bool IsEnd = false;
-    // Start is called before the first frame update
-    void Start()
-    {
 
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        //if (manager.CurAliceHP <= 0)
-        //{
-        //    IsEnd = true;
-        //    main.SetActive(true);
-        //}
-        //else
-        //{
-        //    main.SetActive(false);
-        //    IsEnd = false;
-        //}
-        //if (Input.GetKeyDown(KeyCode.Space))
-        //{
-        //    if(IsEnd == true)
-        //    {
-        //        SceneManager.LoadScene("StartScene");
-
-        //    }
-        //}
-    }
 
    public  void GotoMain()
     {
