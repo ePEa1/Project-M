a092e3d490ec08c85680de225e18a26019b6e3f1
yean123123 <54879918+yean123123@users.noreply.github.com>
2020-06-09 18:10:18 +0900

    카메라 쉐이크

--- Assets/MonsterSystem/Scripts/CameraShakeManager.cs
+++ Assets/MonsterSystem/Scripts/CameraShakeManager.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[SerializeField]
 public class CameraShakeManager : MonoBehaviour
 {
+    public AnimationCurve[] curves;
     // Start is called before the first frame update
     void Start()
     {
