932d84cefd14f139544c73801d8aff69cc146a23
byss <yean123123@github.com>
2020-06-10 12:26:54 +0900

    카메라 쉐이크 구현
    커브값안넣고 랜덤 값으로 구현함

--- Assets/MonsterSystem/Scripts/CameraShakeManager.cs
+++ Assets/MonsterSystem/Scripts/CameraShakeManager.cs
@@ -2,19 +2,40 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[SerializeField]
+
 public class CameraShakeManager : MonoBehaviour
 {
-    public AnimationCurve[] curves;
-    // Start is called before the first frame update
+    [SerializeField] public float shakeAmount;//쉐이크 최대 값
+    [SerializeField] public float duration;//주기
+
+    Vector3 originPos;
+
     void Start()
     {
-        
+
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        originPos = transform.position;
+
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            StartCoroutine(Shake(shakeAmount, duration));
+        }
+    }
+
+    public IEnumerator Shake(float _amount, float _duration)
+    {
+        float timer = 0;
+        while (timer <= _duration)
+        {
+            transform.localPosition = (Vector3)Random.insideUnitCircle * _amount + originPos;
+
+            timer += Time.deltaTime;
+            yield return null;
+        }
+        transform.localPosition = originPos;
+
     }
 }
