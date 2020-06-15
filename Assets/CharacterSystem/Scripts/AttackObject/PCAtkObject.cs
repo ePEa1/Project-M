f0c9c617461005b699a7a08adee5a60eb8818ebc
ePEa_SC <zmzm5442@gmail.com>
2020-06-16 05:30:06 +0900

    패턴추가

---
+++ Assets/CharacterSystem/Scripts/AttackObject/PCAtkObject.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ProjectM.ePEa.CamSystem;
+
+[CreateAssetMenu(fileName = "PC Attack Object", menuName = "CharacterSystem/Attack Object", order = int.MaxValue)]
+public class PCAtkObject : ScriptableObject
+{
+    public float distance;
+    public AnimationCurve distanceCurve;
+    public float rushSpeed;
+    public Collider range;
+    public GameObject[] effs;
+    public Vector3[] effPos;
+    public Vector3[] effDir;
+    public AudioSource[] sfx;
+    public CustomShaking shakeData;
+}
