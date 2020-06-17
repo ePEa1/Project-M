970436113a8cdaf46f576771f9e860578f13167e
yean123123 <54879918+yean123123@users.noreply.github.com>
2020-06-17 14:24:40 +0900

    튜토리얼전 페이드 인아웃 구현(중)

---
+++ Assets/MonsterSystem/Scripts/TutorialTest/TutorialMainManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TutorialMainManager : MonoBehaviour
+{
+
+    public GameObject player;
+
+    [SerializeField] GameObject PlayerUI;
+    [SerializeField] GameObject SpawnMonster;
+    [SerializeField] Image DialogueBox;
+    [SerializeField] Text DialogueText;
+    [SerializeField] GameObject Potal;//튜토리얼 끝날 때 돌아가기
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
